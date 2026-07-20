32d739bd3e59e1f260155d0aab8a3331ce4774b5
utamaabi <abidiutama@gmail.com>
2026-07-20 17:54:06 +0700

    coba lagi terakhir

--- app/components/GroupBtn.tsx
+++ app/components/GroupBtn.tsx
@@ -2,7 +2,6 @@ export default function GroupBtn() {
   return (
       <div className="flex gap-3">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-<<<<<<< HEAD
           repels
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -10,15 +9,6 @@ export default function GroupBtn() {
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           keren
-=======
-          HOME
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          BERANDA
-        </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          IZIN
->>>>>>> b788b9ef94aa5815091beac7a770d014cd546d5b
         </button>
       </div>
   );
