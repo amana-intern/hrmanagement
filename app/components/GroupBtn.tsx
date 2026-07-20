b788b9ef94aa5815091beac7a770d014cd546d5b
adfdhli7 <adfdhli017@student.ub.ac.id>
2026-07-20 17:41:55 +0700

    test ubah dalam

--- app/components/GroupBtn.tsx
+++ app/components/GroupBtn.tsx
@@ -2,13 +2,13 @@ export default function GroupBtn() {
   return (
       <div className="flex gap-3">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Home
+          HOME
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Beranda
+          BERANDA
         </button>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Cuti
+          IZIN
         </button>
       </div>
   );
